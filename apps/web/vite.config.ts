c0f0a56205ffc2f0208bc2f104bd71fa166a66f5
nzhtrtlc <20neziht@gmail.com>
2026-03-10 20:14:01 +0300

    path resolve issue fix.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,5 +9,11 @@ export default defineConfig({
    plugins: [react(), tailwindcss()],
    define: {
       APP_VERSION: JSON.stringify(packageJson.version),
+   },
+   resolve: {
+      alias: {
+         "@packages/types": path.resolve(__dirname, "../../packages/types/index.ts"),
+         "@packages/utils": path.resolve(__dirname, "../../packages/utils/index.ts"),
+      },
    },
 });
